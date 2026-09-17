53235e26712d6431fb9550a0ab7e2e4557a76a95
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-17 15:34:24 -0700

    save-documents-in-db

--- controllers/posts-controllers.js
+++ controllers/posts-controllers.js
@@ -1,25 +1,19 @@
 const { v4: uuidv4 } = require('uuid')
 const { validationResult } = require('express-validator')
 
-let posts = [
-    {
-        id: 'p1',
-        title: 'Title',
-        content: 'Content',
-    },
-]
+const Post = require('../models/posts')
 
 const getPostById = (req, res, next) => {
-    const postId = req.params.pid
+    // const postId = req.params.pid
 
-    const post = posts.find((item) => {
-        return item.id === postId
-    })
+    // const post = posts.find((item) => {
+    //     return item.id === postId
+    // })
 
-    res.json({ post })
+    // res.json({ post })
 }
 
-const createPost = (req, res, next) => {
+const createPost = async (req, res, next) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
@@ -28,19 +22,22 @@ const createPost = (req, res, next) => {
 
     const { title, content } = req.body
 
-    const createdPost = { id: uuidv4(), title, content }
 
-    posts.push(createdPost)
+    // const createdPost = { id: uuidv4(), title, content }
+    const createdPost = new Post({ title, content })
+
+    // posts.push(createPost)
+    await createdPost.save()
 
     res.status(201).json({ post: createdPost })
 }
 
 const deletePost = (req, res, next) => {
-    const postId = req.params.pid
+    // const postId = req.params.pid
 
-    posts = posts.filter((item) => item.id !== postId)
+    // posts.filter((item) => item.id !== postId)
 
-    res.status(200).json({ message: 'Post Deleted.' })
+    // res.status(200).json({ message: 'Post Deleted.' })
 }
 
 exports.getPostById = getPostById
