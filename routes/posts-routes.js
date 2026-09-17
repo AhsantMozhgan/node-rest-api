0cc6a2b7ad3ea085857e2898d94e82de5b0f1aac
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-17 16:08:00 -0700

    route-protection

--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const { check } = require('express-validator')
 
+const auth = require('../middleware/auth')
+
 const router = express.Router()
 const postsControllers = require('../controllers/posts-controllers')
 
 router.get('/:pid', postsControllers.getPostById)
+
+//order is important
+router.use(auth)
 
 router.post(
     '/',
@@ -12,7 +17,6 @@ router.post(
     check('content').isLength({ min: 5 })],
     postsControllers.createPost
 )
-
 
 router.delete('/:pid', postsControllers.deletePost)
 
