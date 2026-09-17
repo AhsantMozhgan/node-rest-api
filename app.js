f9c809f98acf09a3ab3a7666e11fbc7d72811d09
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-17 12:02:03 -0700

    filter-routes

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const postsRoutes = require('./routes/posts-routes')
 
 const app = express()
 
-app.use(postsRoutes)
+app.use('/api/posts', postsRoutes)
 
 app.listen(5000)
--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -2,8 +2,23 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    res.json({ message: 'Post Route' })
+const posts = [
+    {
+        id: 'p1',
+        title: 'Title',
+        content: 'Content',
+    },
+]
+
+router.get('/:pid', (req, res, next) => {
+    const postId = req.params.pid
+
+    const post = posts.find((item) => {
+        return item.id === postId
+    })
+    // res.json( {post: post} )
+    //OR 
+    res.json({ post })
 })
 
 module.exports = router
