8bf387a169b9da15dae4927bb24374e602491192
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-17 15:58:15 -0700

    hash-password

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,18 +1,18 @@
-const { v4: uuidv4 } = require('uuid')
+const bcrypt = require('bcryptjs')
 const User = require('../models/users')
 
 const getUsers = async (req, res, next) => {
-const users = await User.find()
+    const users = await User.find()
+
     res.json({ users })
 }
 
 const signup = async (req, res, next) => {
     const { email, password } = req.body
 
-    // const newUser = { id: uuidv4(), email, password }
-    const newUser = new User({ email, password })
+    const hashedPassword = await bcrypt.hash(password, 12)
+    const newUser = new User({ email, password: hashedPassword })
 
-    // users.push(newUser)
     await newUser.save()
 
     res.status(201).json({ user: newUser })
@@ -23,8 +23,14 @@ const login = async (req, res, next) => {
 
     const validUser = await User.findOne({ email: email })
 
-    if (!validUser || validUser.password !== password) {
-        res.json({ message: 'User is not valid.' })
+    if (!validUser) {
+        return res.json({ message: 'User is not valid.' })
+    }
+
+    const validPassword = await bcrypt.compare(password, validUser.password)
+
+    if (!validPassword) {
+        return res.json({ message: 'Password is not valid.' })
     }
 
     res.json({ message: 'Logged in.' })
